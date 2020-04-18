afbf9c9be55e8faed212dfe55a1b3736a9d3331d
Fulvio <mignano.1810986@studenti.uniroma1.it>
2020-04-18 11:57:15 +0200

    First commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+//imports
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//initializing server
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+
+
+//starting server
+const port = process.env.PORT || 5000;
+app.listen(port, () => { console.log(`Sever started on port ${port}`)});
