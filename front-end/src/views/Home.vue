4438d4e114336d58c0e377f9f672786251b450a8
Fulvio <mignano.1810986@studenti.uniroma1.it>
2020-04-24 08:55:12 +0200

    Aggiunte immagini alla landing page

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,26 +1,16 @@
 <template>
   <div>
-    <b-container>
-      <b-row>
-        <b-col md="6">
-          <Status />
-        </b-col>
-        <b-col md="6">
-          <Status />
-        </b-col>
-      </b-row>
-    </b-container>
+    <Carousel />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Status from "@/components/Status.vue";
+import Carousel from "@/components/Carousel.vue";
 
 export default {
   name: "Home",
   components: {
-    Status
+    Carousel
   }
 };
 </script>
