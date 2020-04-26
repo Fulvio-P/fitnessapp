b5c2b1b8dc6c5cb9ef00b4e5693452e4a88d247f
Fulvio <mignano.1810986@studenti.uniroma1.it>
2020-04-26 12:28:23 +0200

    Provato a fare una sidebar con css nella pagina about

---
+++ front-end/src/components/SideNavbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="SideNav">
+    <ul class="SideNavList">
+      <li class="SideNavItem">
+        <a href="#" class="SideNavLink">
+          <img src="../assets/icons/user-profile.svg" />
+          <span class="LinkText">Food</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "SideNav"
+};
+</script>
+
+<style scoped>
+.SideNav {
+  font-size: 16px;
+  width: 5rem;
+  height: 100vh;
+  position: fixed;
+  background: pink;
+}
+
+.SideNavList {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.SideNavItem {
+  width: 100%;
+}
+
+.SideNavItem:last-child {
+  margin-top: auto;
+}
+
+.SideNavLink {
+  display: flex;
+  height: 5rem;
+  text-decoration: none;
+  align-items: center;
+}
+</style>
