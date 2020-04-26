a427698bc34a9d7ebf294591facd469d257fb67e
torchipeppo <mariobrosforever@gmail.com>
2020-04-26 15:40:56 +0200

    aggiunti width/height

--- front-end/src/components/SvgTest.vue
+++ front-end/src/components/SvgTest.vue
@@ -7,6 +7,8 @@
       xmlns:xlink="http://www.w3.org/1999/xlink"
       x="0px"
       y="0px"
+      width="100%"
+      height="100%"
       viewBox="0 0 19.738 19.738"
       style="enable-background:new 0 0 19.738 19.738;"
       xml:space="preserve"
