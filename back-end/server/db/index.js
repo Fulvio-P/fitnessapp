4b2ced87739f1fd94d08c7b6681a23ad67c08867
torchipeppo <mariobrosforever@gmail.com>
2020-04-28 20:15:23 +0200

    database ristrutturato (TODO calorie)

--- back-end/server/db/index.js
+++ back-end/server/db/index.js
@@ -10,6 +10,9 @@ https://node-postgres.com/features/connecting
 https://node-postgres.com/features/queries
 https://node-postgres.com/features/pooling
 */
+
+//TODO: tabella calorie
+
 const { Pool } = require("pg");
 const pool = new Pool();
 
@@ -18,38 +21,42 @@ require("dotenv").config();
 //crea un nuovo utente nel DB di test
 async function newUser(username, password) {
     try {
-        var res = await pool.query("INSERT INTO accesso(username, password) VALUES ($1, $2);",
+        var res = await pool.query("INSERT INTO utente(username, password) VALUES ($1, $2);",
                                     [username, password]);
-        console.log("insert "+username+" in accesso: "+res.rows);
-        res = await pool.query("INSERT INTO nutrizione(username) VALUES ($1);", [username]);
-        console.log("insert "+username+" in nutrizione: "+res.rows);
+        console.log("insert "+username+" in utente: "+res.rows);
     } catch(err) {
         console.error("newUser("+username+","+password+"): "+err.stack);
     }
 }
 
-//imposta i valori di nutrizione di un utente se la sua password è corretta
-async function setNutrition(username, password, zuccheri, proteine, grassi) {
-    var queryRes = await pool.query("SELECT * FROM accesso WHERE username=$1", [username]);
+async function getId(username) {
+    var queryRes = await pool.query("SELECT * FROM utente WHERE username=$1", [username]);
     if (queryRes.rows.length<1) {
-        return "setNutrition "+username+": L'utente non esiste";
+        throw new Error("getId "+username+": L'utente non esiste");
     }
-    if (queryRes.rows[0].password!=password) {
-        return "setNutrition "+username+": Password sbagliata";
+    //console.log(queryRes.rows);
+    return queryRes.rows[0].id;
+}
+
+//aggiunge/modifica una misura di peso di un utente in una data
+async function setPeso(username, data, peso) {
+    var id;
+    try {
+        id = await getId(username);
+    } catch (err) {
+        return "setPeso: "+err.message;
     }
-    queryRes = await pool.query("SELECT * FROM nutrizione WHERE username=$1", [username]);
-    if (queryRes.length<1) {
-        let res = await pool.query("INSERT INTO nutrizione VALUES ($1,$2,$3,$4)", 
-                        [username, zuccheri, proteine, grassi]);
-        return "setNutrition "+username+": "+res.rows+"(insert)";
+    var queryRes = await pool.query("SELECT * FROM misuraPeso WHERE id=$1 AND data=$2", 
+                                    [id, data]);
+    if (queryRes.rows.length<1) {
+        let res = await pool.query("INSERT INTO misuraPeso VALUES ($1,$2,$3)", 
+                                   [id, data, peso]);
+        return "setPeso "+id+": "+res.rows+"(insert)";
     }
-    let res =  await pool.query("UPDATE nutrizione SET username=$1, "+
-                                                        "carboidrati=$2, "+
-                                                        "proteine=$3, "+
-                                                        "grassi=$4"+
-                                                "WHERE username=$1",
-                                [username, zuccheri, proteine, grassi]);
-    return "setNutrition "+username+": "+res.rows+"(update)";
+    let res =  await pool.query("UPDATE misuraPeso SET peso=$3 "+
+                                                  "WHERE id=$1 AND data=$2",
+                                [id, data, peso]);
+    return "setPeso "+id+": "+res.rows+"(update)";
 }
 
 /*
@@ -61,36 +68,37 @@ Vedremo poi quante e quali tabelle ci servono.
 */
 async function initDB() {
     console.log("N.B: Avere risposte vuote per insert/update è buon segno!");
-    await pool.query("CREATE TABLE accesso ("+
-                        "username VARCHAR(50) PRIMARY KEY, "+
+    await pool.query("CREATE TABLE utente ("+
+                        "id SERIAL PRIMARY KEY, "+
+                        "username VARCHAR(50) UNIQUE NOT NULL, "+
                         "password VARCHAR(50) NOT NULL);"
     );
-    await pool.query("CREATE TABLE nutrizione ("+
-                        "username VARCHAR(50) PRIMARY KEY REFERENCES accesso(username), "+
-                        "carboidrati NUMERIC NOT NULL DEFAULT 0, "+
-                        "proteine NUMERIC NOT NULL DEFAULT 0, "+
-                        "grassi NUMERIC NOT NULL DEFAULT 0);"
+    await pool.query("CREATE TABLE misuraPeso ("+
+                        "id integer NOT NULL REFERENCES utente(id), "+
+                        "data DATE NOT NULL DEFAULT CURRENT_DATE, "+    //DATE è solo giorno, senza ora, e con questo DEFAULT possiamo prendere in automatico la data corrente.
+                        "peso NUMERIC NOT NULL, "+
+                        "PRIMARY KEY (id, data));"    //solo una misurazione al giorno per utente
     );
     await newUser("AkihikoSanada", "polydeuces");
     await newUser("ChieSatonaka", "tomoe");
     await newUser("EdelgardVonHresvelg", "blackeagle");
-    console.log(await setNutrition("AkihikoSanada", "polydeuces", 10, 1000, 5));   //ok
-    console.log(await setNutrition("ChieSatonaka", "tomoe", 20, 20, 20));    //ok
-    console.log(await setNutrition("EdelgardVonHresvelg", "blackeagle", 100, 10, 5));   //ok
-    console.log(await setNutrition("ChieSatonaka", "jiraiya", 10000, 0, 10000));   //no password
-    console.log(await setNutrition("LukeTriton", "puzzles<3", 10, 10, 10));   //no utente
+    console.log(await setPeso("AkihikoSanada", new Date("2020-04-25"), 80));
+    console.log(await setPeso("AkihikoSanada", new Date("2020-04-26"), 70));
+    console.log(await setPeso("AkihikoSanada", new Date("2020-04-27"), 75));
+    console.log(await setPeso("EdelgardVonHresvelg", new Date("2020-04-27"), 60));
+    console.log(await setPeso("LukeTriton", new Date("2020-04-27"), 50));   //no utente
 }
 
 //cancella le tabelle del DB, per permettermi di ricominciare da capo
 //ancora, è una funzione irrealistica che mi serve per giocare un po'
 async function destroyDB() {
     try {
-        await pool.query("DROP TABLE nutrizione;");
+        await pool.query("DROP TABLE misuraPeso;");
     } catch (err) {
         ;
     }
     try {
-        await pool.query("DROP TABLE accesso;");
+        await pool.query("DROP TABLE utente;");
     } catch (err) {
         ;
     }
