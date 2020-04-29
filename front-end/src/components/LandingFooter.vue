b54a2886be04e3b495ef6f3e3384731de433cec4
Fulvio <mignano.1810986@studenti.uniroma1.it>
2020-04-29 17:45:48 +0200

    Creato footer responsivo

--- front-end/src/components/LandingFooter.vue
+++ front-end/src/components/LandingFooter.vue
@@ -1,11 +1,58 @@
 <template>
   <div class="footer">
-    <p>
-      Questo è un footer, qua possiamo mettere link a tipo la pagina github e
-      info nostre per vantarci, <br />
-      per adesso è giallo perchè ho scritto un hex a caso<br />
-      Anche qui dovrebbe bastare HTML e CSS
-    </p>
+    <b-container 
+    class="footer-content"
+    fluid
+    >
+      <b-row>
+        <b-col sm class="footer-section about">
+          <h1 class="logo-text">FitnessApp</h1>
+          <p>
+            Una semplice applicazione per registrare
+            dati sul proprio benessere e fitness. La
+            versione attuale è presentata come progetto
+            per il corso di Linguaggi e Teconolgie Web.
+          </p>
+        </b-col>
+        <b-col sm class="footer-section links">
+          <h1>Link Rapidi</h1>
+          <ul>
+            <li><a href="#">Servizi</a></li>
+            <li><a href="#">Team</a></li>
+            <li><a href="https://github.com/Fulvio-P/fitnessapp" class="git-hub">Pagina GitHub</a></li>
+            <li><a href="#">Termini e condizioni</a></li>
+          </ul>
+        </b-col>
+        <b-col sm class="footer-section contact-form">
+          <h1>Contattaci</h1>
+          <b-form>
+            <b-form-group
+            label="Indirizzo e-mail"
+            label-for="input-1"
+            description="Non condiveremo il tuo indirizzo e-mail con terze parti"
+            >
+              <b-form-input
+              id="input-1"
+              type="email"
+              required
+              placeholder="pippo@email.com"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group>
+              <b-form-textarea
+              id="input-textarea"
+              placeholder="7/10 too much water"
+              no-resize
+              ></b-form-textarea>
+            </b-form-group>
+            <b-button type="submit" variant="primary">Invia</b-button>
+          </b-form>
+        </b-col>
+      </b-row>
+    </b-container>
+    <div class="footer-bottom">
+      Fulvio Pio Mignano | Francesco Petri
+    </div>
   </div>
 </template>
 
@@ -18,5 +65,17 @@ export default {
 <style scoped>
 .footer {
   background: #ffff00;
+}
+
+.footer .footer-content .footer-section {
+  padding: 25px;
+}
+
+.footer .footer-bottom {
+  background: #ffffff;
+  height: 50px;
+  width: 100%;
+  text-align: center;
+  padding-top: 20px;
 }
 </style>
