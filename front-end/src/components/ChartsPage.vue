b8e777e0c6ebdf5b2bf325362caa741db41edf3a
torchipeppo <mariobrosforever@gmail.com>
2020-04-29 16:39:23 +0200

    Adesso il grafico accetta dati parametricamente

--- front-end/src/components/ChartsPage.vue
+++ front-end/src/components/ChartsPage.vue
@@ -1,16 +1,32 @@
 <template>
     <div>
-        <WeightChart />
+        <WeightChart :chartdata="chartdata" :options="options"/>
     </div>
 </template>
 
 <script>
 import WeightChart from "@/components/WeightChart.vue";
 
+var chartdata = {
+    labels: ['January', 'February'],
+    datasets: [
+        {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 20]
+        }
+    ]
+};
+var options = {
+    responsive: true,
+    maintainAspectRatio: false
+}
+
 export default {
   name: "ChartsPage",
   components: {
       WeightChart
-  }
+  },
+  data: () => ({chartdata, options})   //le parentesi tonde servono a far interpretare le graffe come Object, e non come blocco di istruzioni
 };
 </script>
--- front-end/src/components/WeightChart.vue
+++ front-end/src/components/WeightChart.vue
@@ -8,22 +8,7 @@ import { Line } from 'vue-chartjs'
 
 export default {
     extends: Line,
-    data: () => ({
-        chartdata: {
-            labels: ['January', 'February'],
-            datasets: [
-                {
-                    label: 'Data One',
-                    backgroundColor: '#f87979',
-                    data: [40, 20]
-                }
-            ]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false
-        }
-    }),
+    props: ["chartdata", "options"],
     mounted() {
         this.renderChart(this.chartdata, this.options)
     }
