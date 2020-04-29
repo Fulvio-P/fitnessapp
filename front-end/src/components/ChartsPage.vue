184f38eb98ed9e4a1e3efe3daf87eb2f8b073eeb
torchipeppo <mariobrosforever@gmail.com>
2020-04-29 20:26:07 +0200

    Seconda parte del commit precedente, che apparentemente mi ero perso

--- front-end/src/components/ChartsPage.vue
+++ front-end/src/components/ChartsPage.vue
@@ -6,6 +6,20 @@
 
 <script>
 import WeightChart from "@/components/WeightChart.vue";
+const axios = require("axios").default;
+
+function getPesi(id) {
+    axios.get("http://localhost:5000/api/weight/"+id)
+        .then(resp => {
+            var serverData = resp.data.dataPoints
+            var chartData = chartdata.datasets[0].data = [];
+            serverData.forEach(v => chartData.push({
+                x: v.data,
+                y: v.peso
+            }));
+            console.log(chartData);
+        });
+}
 
 var chartdata = {
     datasets: [
@@ -30,6 +44,14 @@ export default {
   components: {
       WeightChart
   },
-  data: () => ({chartdata, options})   //le parentesi tonde servono a far interpretare le graffe come Object, e non come blocco di istruzioni
+  data: () => ({chartdata, options}),   //le parentesi tonde servono a far interpretare le graffe come Object, e non come blocco di istruzioni
+  computed: {
+      loggedId() {
+          return this.$store.state.loggedId
+      }
+  },
+  mounted() {
+    getPesi(this.loggedId);
+  }
 };
 </script>
