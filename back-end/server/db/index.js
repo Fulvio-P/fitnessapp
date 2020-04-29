ab9ec2eac727cff8aa65767b9230b23265b9495d
torchipeppo <mariobrosforever@gmail.com>
2020-04-29 15:30:58 +0200

    Separati comandi da esportare al resto dell'app e comandi solo di debug

--- back-end/server/db/index.js
+++ back-end/server/db/index.js
@@ -11,8 +11,6 @@ https://node-postgres.com/features/queries
 https://node-postgres.com/features/pooling
 */
 
-//TODO: tabella calorie
-
 const { Pool } = require("pg");
 const pool = new Pool();
 
@@ -79,104 +77,11 @@ async function setCalorie(username, data, calin, calout) {
     return "setCalorie "+id+": "+res.rows+"(update)";
 }
 
-/*
-Inizializza le tabelle di un DB di test.
-Ovviamente una funzione del genere non può esistere in produzione, ma in qualche modo
-devo partire.
-*/
-async function initDB() {
-    console.log("N.B: Avere risposte vuote per insert/update è buon segno!");
-    await pool.query("CREATE TABLE utente ("+
-                        "id SERIAL PRIMARY KEY, "+
-                        "username VARCHAR(50) UNIQUE NOT NULL, "+
-                        "password VARCHAR(50) NOT NULL);"
-    );
-    await pool.query("CREATE TABLE misuraPeso ("+
-                        "id integer NOT NULL REFERENCES utente(id), "+
-                        "data DATE NOT NULL DEFAULT CURRENT_DATE, "+    //DATE è solo giorno, senza ora, e con questo DEFAULT possiamo prendere in automatico la data corrente.
-                        "peso NUMERIC NOT NULL, "+
-                        "PRIMARY KEY (id, data));"    //solo una misurazione al giorno per utente
-    );
-    await pool.query("CREATE TABLE misuraCalorie ("+
-                        "id integer NOT NULL REFERENCES utente(id), "+
-                        "data DATE NOT NULL DEFAULT CURRENT_DATE, "+
-                        "calin NUMERIC NOT NULL DEFAULT 0, "+
-                        "calout NUMERIC NOT NULL DEFAULT 0, "+
-                        "PRIMARY KEY (id, data));"
-    );
-    await newUser("AkihikoSanada", "polydeuces");
-    await newUser("ChieSatonaka", "tomoe");
-    await newUser("EdelgardVonHresvelg", "blackeagle");
-    console.log(await setPeso("AkihikoSanada", new Date("2020-04-25"), 80));
-    console.log(await setPeso("AkihikoSanada", new Date("2020-04-26"), 70));
-    console.log(await setPeso("AkihikoSanada", new Date("2020-04-27"), 75));
-    console.log(await setPeso("EdelgardVonHresvelg", new Date("2020-04-27"), 60.005));
-    console.log(await setPeso("LukeTriton", new Date("2020-04-27"), 50));   //no utente
-    console.log(await setCalorie("ChieSatonaka", new Date("2020-04-25"), 100, 100));
-    console.log(await setCalorie("ChieSatonaka", new Date("2020-04-26"), 50, 0));
-    console.log(await setCalorie("ChieSatonaka", new Date("2020-04-27"), 0, 60));
-    console.log(await setCalorie("EdelgardVonHresvelg", new Date("2020-04-27"), 150.25, 200));
-    console.log(await setCalorie("JunpeiIori", new Date("2020-04-27"), 0, 0));   //no utente
+module.exports = {
+    getId,
+    newUser,
+    setCalorie,
+    setPeso
 }
 
-//cancella le tabelle del DB, per permettermi di ricominciare da capo
-//ancora, è una funzione irrealistica che mi serve per giocare un po'
-async function destroyDB() {
-    try {
-        await pool.query("DROP TABLE misuraPeso;");
-    } catch (err) {
-        ;
-    }
-    try {
-        await pool.query("DROP TABLE misuraCalorie;");
-    } catch (err) {
-        ;
-    }
-    try {
-        await pool.query("DROP TABLE utente;");
-    } catch (err) {
-        ;
-    }
-}
-
-//mi permette di scrivere interattivamente tutte le query che voglio
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-function questionPromise(rl) {
-    return new Promise((resolve, reject) => {
-        rl.question("PG> ", ans=>{
-            if (ans=="quit") {
-                resolve(true);
-            }
-            else if (ans=="") {
-                resolve(false);
-            }
-            else {
-                pool
-                    .query(ans)
-                    .then(res=>{console.log(res.rows);resolve(false);})
-                    .catch(err=>{console.error("shell query "+ans+": "+err.stack);resolve(false);})
-            }
-        });
-    });
-}
-async function shell() {
-    await destroyDB();   //resetta in caso il server sia crashato l'ultima volta
-    try {
-        await initDB();
-    } catch(err) {
-        console.error("crashato initDB:\n"+err.message);
-        return;
-    }
-    var stop=false;
-    while(!stop) {
-        stop = await questionPromise(rl);
-    }
-    await destroyDB();
-    rl.close();
-}
-
-shell();
+//shell();
