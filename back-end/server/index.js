a8e9af12dede56071e271928af263f975f16ce64
Fulvio <mignano.1810986@studenti.uniroma1.it>
2020-05-02 12:08:26 +0200

    Aggiunti handler di prova

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -12,3 +12,17 @@ app.use(bodyParser.json());
 //starting server
 const port = process.env.PORT || 5000;
 app.listen(port, () => { console.log(`Sever started on port ${port}`)});
+
+
+//handling requests
+app.post("/weight" , (req, res) => {
+    console.log(req);
+})
+
+app.post("/food", (req, res) => {
+    console.log(req);
+})
+
+app.post("/activities", (req, res) => {
+    console.log(req);
+})
