6fe5e80d785bac5ced8731c73cf32a7925ff29e0
fulvio <placeholder@mail.com>
2020-05-03 21:00:44 +0200

    Formattazione

--- back-end/server/routes/auth.js
+++ back-end/server/routes/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 
-//POST : riceve dati e crea un utente nel database se non esiste già
+//POST api/user/register: riceve dati e crea un utente nel database se non esiste già
 router.post('/register', async (req, res) => {
 
     /*estraggo dati dalla richiesta*/
@@ -13,17 +13,16 @@ router.post('/register', async (req, res) => {
     let password = req.body.password;
 
 
-    
-    
-    
+
+
+
     /* Controllo che l'utente non sia già registrato */
     try {
         let user = await db.getUserByEmail(email);
         if(user) return res.status(400).send("Utente già registrato");
     } catch (err) {
         res.status(500).send("Il controllo utente già registrato ha dato errore: "+err);
-    } 
-
+    }
 
 
 
@@ -32,10 +31,10 @@ router.post('/register', async (req, res) => {
     /* Hashing della password */
     let salt = bcrypt.genSaltSync(10);
     let hashedPassword = bcrypt.hashSync(password, salt);
+    
+    
 
-    
-    
-    
+
     
     /* Provo ad aggiungere l'utente al database */
     try {
