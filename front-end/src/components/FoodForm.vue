198e9ff69fb92d6bd090daec3508f7999bd81d18
torchipeppo <mariobrosforever@gmail.com>
2020-05-03 15:23:23 +0200

    lint

--- front-end/src/components/FoodForm.vue
+++ front-end/src/components/FoodForm.vue
@@ -1,62 +1,58 @@
 <template>
-    <div class="food-form">
-        <b-form @submit="sendFood">
+  <div class="food-form">
+    <b-form @submit="sendFood">
+      <!-- Cibo mangiato -->
+      <b-form-input
+        id="food-input"
+        v-model="foodItem"
+        placeholder="Cosa hai mangiato ?"
+        type="search"
+        required
+      ></b-form-input>
 
-            <!-- Cibo mangiato -->
-            <b-form-input
-            id="food-input"
-            v-model="foodItem"
-            placeholder="Cosa hai mangiato ?"
-            type="search"
-            required
-            ></b-form-input>
+      <!-- Quantità del cibo -->
+      <b-form-input
+        id="food-input"
+        v-model="foodQuantity"
+        placeholder="Quanti grammi ?"
+        type="number"
+        min="0"
+        max="1000"
+      ></b-form-input>
 
-            <!-- Quantità del cibo -->
-            <b-form-input
-            id="food-input"
-            v-model="foodQuantity"
-            placeholder="Quanti grammi ?"
-            type="number"
-            min="0"
-            max="1000"
-            ></b-form-input>
+      <b-form-input
+        id="food-energy"
+        v-model="foodEnergy"
+        placeholder="350"
+        type="number"
+        required
+      ></b-form-input>
 
-            <b-form-input
-            id="food-energy"
-            v-model="foodEnergy"
-            placeholder="350"
-            type="number"
-            required
-            ></b-form-input>
-
-            <b-button type="submit">Regitra calorie</b-button>
-            
-        </b-form>
-        
-    </div>
+      <b-button type="submit">Regitra calorie</b-button>
+    </b-form>
+  </div>
 </template>
 
-
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-    name:"FoodForm",
-    data(){
-        return{
-            foodItem: undefined,
-            foodQuantity: undefined,
-            foodEnergy: undefined
-        }
-    },
-    methods: {
-        sendFood(){
-            axios.post("http://localhost:5000/food",{
-                foodItem: this.foodItem,
-                foodQuantity: this.foodQuantity,
-                foodEnergy: this.foodEnergy
-            })
-        }
-    },
-}
+  name: "FoodForm",
+  data() {
+    return {
+      foodItem: undefined,
+      foodQuantity: undefined,
+      foodEnergy: undefined
+    };
+  },
+  methods: {
+    sendFood() {
+      axios.post("http://localhost:5000/food", {
+        foodItem: this.foodItem,
+        foodQuantity: this.foodQuantity,
+        foodEnergy: this.foodEnergy
+      });
+    }
+  }
+};
 </script>
