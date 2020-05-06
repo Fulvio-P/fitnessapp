d6fbdaa20dd3c6c90f727c90db33e4ceef864346
torchipeppo <mariobrosforever@gmail.com>
2020-05-06 11:27:28 +0200

    Primo tentativo di uso token, non ancora testato

--- back-end/server/routes/weight.js
+++ back-end/server/routes/weight.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const db = require("../db/index");
+const jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET;
 
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
+router.get("/:token", async (req, res) => {
+    var decoded;
     try {
-        var rows = await db.getMisurePeso(req.params.id);
+        decoded = jwt.verify(req.params.token, jwtSecret);
+    }
+    catch (err) {
+        if (err.name=="TokenExpiredError") {
+            res.status(400).send("Token Expired");
+        }
+        else {
+            res.status(400).send("Wrong Token");
+        }
+    }
+    try {
+        var rows = await db.getMisurePeso(decoded.id);
     }
     catch (err) {
         return res.status(500).send(err.message);
@@ -14,7 +28,7 @@ router.get("/:id", async (req, res) => {
         return res.status(404).send("User ID not found");
     }
     var toSend = {
-        id: req.params.id,
+        username: decoded.username,
         dataPoints: rows,
     };
     return res.status(200).send(JSON.stringify(toSend));
