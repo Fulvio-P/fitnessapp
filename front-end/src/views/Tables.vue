2919cdd792be3d349c8da46827f517e8a8f5389d
Fulvio-P <mignano.1810986@studenti.uniroma1.it>
2020-05-10 10:10:06 +0200

    Aggiunta vista per le tabelle

---
+++ front-end/src/views/Tables.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tablesview sidebar-margin">
+    <h1>Diario</h1>
+    <FormsPage />
+  </div>
+</template>
+
+<script>
+import FormsPage from "@/components/TablePage.vue";
+export default {
+  name: "Tables",
+  components: {
+    TablePage
+  }
+};
+</script>
+
+<style scoped></style>
