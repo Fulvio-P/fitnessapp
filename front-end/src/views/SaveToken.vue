0e3d92097bd53322880de470a76dec3ded6135b2
torchipeppo <mariobrosforever@gmail.com>
2020-05-15 12:29:25 +0200

    Mi ero dimenticato di aggiungere il file savetoken

---
+++ front-end/src/views/SaveToken.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Login riuscito! Caricamento in corso...
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SaveToken",
+  created() {
+    this.$store
+      .dispatch("AUTH_SAVETOKEN", this.$route.query.token)
+      .then(()=>{
+        this.$router.push("/charts")
+      });
+  }
+};
+</script>
+
+<style scoped></style>
