df23f2775e45f9a0c1d64ced992283c237c956e5
torchipeppo <mariobrosforever@gmail.com>
2020-05-16 20:24:53 +0200

    autenticazione per singolo messaggio implementata

--- back-end/server/routes/fitbitsync.js
+++ back-end/server/routes/fitbitsync.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const db = require("../db/index");
 const utils = require("../globalutils");
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
+const jwtSecret = process.env.JWT_SECRET;
+
+//la funzione in globalutils è una funzione middleware che guarda la request,
+//qui è inutile
+function verifyTokenInMessage(ws, msg) {
+    try {
+        var decoded = jwt.verify(msg.token, jwtSecret);
+    }
+    catch (err) {
+        console.error(`jwt ${err.name}: ${err.message}`);   //non sono sicuro che vogliamo rimandare al client il messaggio d'errore di jwt
+        if (err.name=="TokenExpiredError") {
+            ws.send(JSON.stringify({
+                type: "error",
+                message: "Token scaduto"
+            }));
+            return false;
+        }
+        else {
+            ws.send(JSON.stringify({
+                type: "error",
+                message: "Autenticazione fallita"
+            }));
+            return false;
+        }
+    }
+    //se va tutto bene...
+    return decoded;   //dati utente
+}
 
 router.ws("/", function(ws, req) {
     //se ci riuscissimo potremmo autenticare la connessione una volta all'inizio
@@ -13,7 +42,8 @@ router.ws("/", function(ws, req) {
         console.log(msg)
         msg = JSON.parse(msg);
 
-        //TODO controllo token
+        var user = verifyTokenInMessage(ws, msg);
+        if (!user) return;
 
         if (msg.action!="fitbitsync") {
             return ws.send(JSON.stringify({
