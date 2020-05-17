b7d5bf24e9e45449fcd34e72b9295b6b8d3c4ad1
Fulvio-P <mignano.1810986@studenti.uniroma1.it>
2020-05-17 21:43:43 +0200

    creato scheletro per il metodo sync

---
+++ back-end/server/syncWork/syncActivities.js
@@ -0,0 +1,54 @@
+const fitbit = require('../fitbit/fitbit');
+const db = require('../db/index');
+
+
+/*
+    README:
+    questo file contiene le istruzioni per recuperare le info sulle attività da fitbit
+    e inserirle nel database insiame a quelle registrate tramite fitnessApp
+*/
+
+
+function sync(userId){
+
+    return new Promise((resolve, reject)=>{
+
+        //Recupero la data di partenza dal database
+        //TODO: capire come e dove salvare la data per avere il giusto formato
+        //COMMENTO DEV: per il momento test hardcoded
+        let lastChecked = '2020-05-16T10:00:00'
+
+        
+        //Imposto i parrametri della richiesta
+        let requestUrl ='https://api.fitbit.com/1/user/-/activities/list.json?'+
+                        'afterDate='+ lastChecked
+                        '&offset=0'+
+                        '&limit=100'+
+                        '&sort=asc'
+        ;
+
+
+        //Provo a ottenere i dati da fitbit
+        fitbit.get(userId, requestUrl)
+
+        //fallimento: fitbit ha inviato un messaggio di errore
+        .catch(error => {
+            rerject(error);
+        })
+
+        //successo: fitbit ha risposto con dati
+        .then( response =>{
+
+            //Ciclo sulle varie attività nella risposta
+            //Per ogni attività creo un rercord nel database
+
+
+
+            //Accedo al database per aggiornare last checked all'ora attuale
+
+            //una volta finito tutto
+            resolve("activities_synced")
+
+        })
+    })
+}
