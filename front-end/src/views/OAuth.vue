04d204deddc937e219eb96e45f84c8bf44fab0aa
torchipeppo <mariobrosforever@gmail.com>
2020-05-19 20:08:01 +0200

    Fatta view OAuth (parte 2)

---
+++ front-end/src/views/OAuth.vue
@@ -0,0 +1,20 @@
+<template>
+  <LoginViewGeneric>
+    <LoginDialogOAuth />
+  </LoginViewGeneric>
+</template>
+
+<script>
+import LoginDialogOAuth from "@/components/LoginDialogOAuth.vue";
+import LoginViewGeneric from "@/components/LoginViewGeneric.vue";
+
+export default {
+  name: "OAuth",
+  components: {
+    LoginDialogOAuth,
+    LoginViewGeneric
+  }
+};
+</script>
+
+<style scoped></style>
