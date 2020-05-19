f9e78b59fd4e6b51523170be7ece3e0afbd0efc6
Fulvio-P <mignano.1810986@studenti.uniroma1.it>
2020-05-19 23:10:20 +0200

    Aggiunto endpoint developer GET

--- back-end/server/routes/developer.js
+++ back-end/server/routes/developer.js
@@ -5,5 +5,39 @@ const utils = require("../globalutils");
 const router = express.Router();
 
 
+router.use(utils.verifyJWT);
+
+
+router.get("/", async(req, res)=>{
+
+    //estraggo dati richiesta
+    const userId = req.user.id;
+    const username = req.user.username;
+    const internal = req.internalToken;
+
+
+    //le proprie applicazioni sono visualizzabili solo dal front end
+    if(!internal){
+        res.status(403).send("Forbidden");
+    }
+
+
+    //carico le applicazioni dal db
+    try{
+        var rows = await db.getClientByUser(userId);
+    } catch (error) {
+        console.error(`postgres error no. ${err.code}: ${err.message}`);
+        return res.status(500).send("Internal Database Error");
+    }
+
+    const toSend = {
+        username: username,
+        dataPoints: rows,
+    };
+
+    return res.status(200).json(toSend);
+
+})
+
 
 module.exports = router;
