09175f32c96c8352311496fa9ddbfc0db5135270
torchipeppo <mariobrosforever@gmail.com>
2020-05-20 16:24:34 +0200

    form registrazione nuova app

--- front-end/src/views/Developer.vue
+++ front-end/src/views/Developer.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="padding">
         <h1>Pagina developer</h1>
+        <h3>Registra una nuova applicazione</h3>
+        <DeveloperForm />
+        <hr />
+        <h3>Le tue applicazioni</h3>
         <DeveloperTable />
     </div>
 </template>
 
 <script>
+import DeveloperForm from '@/components/DeveloperForm.vue';
 import DeveloperTable from '@/components/DeveloperTable.vue';
 
 export default {
     components:{
-        DeveloperTable,
+        DeveloperForm,
+        DeveloperTable
     }    
 }
 </script>
