85bb5dd1c9dea19acfa850b35959aeec2a0e9d1d
torchipeppo <mariobrosforever@gmail.com>
2020-05-20 16:44:38 +0200

    Aggiunto pulsante per tornare indietro

--- front-end/src/views/Developer.vue
+++ front-end/src/views/Developer.vue
@@ -1,22 +1,35 @@
 <template>
-    <div class="padding">
-        <h1>Pagina developer</h1>
-        <h3>Registra una nuova applicazione</h3>
-        <DeveloperForm @posted="updateTable" />
-        <hr />
-        <h3>Le tue applicazioni</h3>
-        <DeveloperTable ref="devTable" />
+    <div>
+        <b-button class="back-btn" v-router-link to="/profile">
+            <BIconChevronDoubleLeft />
+            Indietro
+        </b-button>
+        <div class="padding">
+            
+            <h1>Pagina developer</h1>
+            <h3>Registra una nuova applicazione</h3>
+            <DeveloperForm @posted="updateTable" />
+            <hr />
+            <h3>Le tue applicazioni</h3>
+            <DeveloperTable ref="devTable" />
+        </div>
+        <b-button class="back-btn" v-router-link to="/profile">
+            <BIconChevronDoubleLeft />
+            Indietro
+        </b-button>
     </div>
 </template>
 
 <script>
 import DeveloperForm from '@/components/DeveloperForm.vue';
 import DeveloperTable from '@/components/DeveloperTable.vue';
+import { BIconChevronDoubleLeft } from "bootstrap-vue";
 
 export default {
     components:{
         DeveloperForm,
-        DeveloperTable
+        DeveloperTable,
+        BIconChevronDoubleLeft
     },
     methods: {
         updateTable() {
@@ -25,3 +38,13 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.no-padding-top {
+    padding-top: 0;
+}
+.back-btn {
+    position: absolute;
+    margin: 0.5rem;
+}
+</style>
