b0cbda3d791c1accef1e43afc5824b88ff876c56
Fulvio-P <mignano.1810986@studenti.uniroma1.it>
2020-05-20 15:13:30 +0200

    Aggiunta tabella dei client

---
+++ front-end/src/components/DeveloperTable.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <b-table :items="items" :fields="fields" striped responsive="sm">
+
+      <template v-slot:cell(delete)="row">
+        <b-button
+          size="sm"
+          @click="deleteClient(row.item.clientid)"
+          class="mr-2 red-btn"
+        >
+          Elimina
+        </b-button>
+      </template>
+
+    </b-table>
+  </div>
+</template>
+
+<script>
+const developerURL = 'http://localhost:5000/api/developer/';
+
+export default {
+  data() {
+    return {
+      fields:[
+        { key: 'clientname', label: 'Nome Applicazione' },
+        { key: 'clientid', label: 'Codice Applicazione' },
+        { key: 'redirect', label: 'Redirect URL' },
+        { key: "delete", label: "" }       
+      ],
+      items:[],
+    };
+  },
+  methods: {
+    deleteClient(key) {
+      let url = developerURL + key;
+      //avvio chiamata API (gestita da vuex)
+      this.$store
+        .dispatch('API_DELETE', url)
+        //se tutto va bene
+        .then(()=>{
+          this.getClients();
+        })
+        //se qualcosa va male
+        .catch(()=>{
+          alert(this.$store.state.status);
+        });
+    },
+    
+    getClients() {
+      this.$store
+        //avvio chiamata API (gestita da vuex)
+        .dispatch("API_GET", developerURL)
+        //se tutto va bene
+        .then(resp => {
+          this.items = resp.data.dataPoints;
+        })
+        //se qualcosa va male
+        .catch(() => {
+          alert(this.$store.state.status);
+        });
+    },
+  },
+  created() {
+    this.getClients();
+  },    
+}
+</script>
--- front-end/src/views/Developer.vue
+++ front-end/src/views/Developer.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="padding">
         <h1>Pagina developer</h1>
+        <DeveloperTable />
     </div>
 </template>
+
+<script>
+import DeveloperTable from '@/components/DeveloperTable.vue';
+
+export default {
+    components:{
+        DeveloperTable,
+    }    
+}
+</script>
