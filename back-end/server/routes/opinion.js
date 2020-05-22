39bf55b3fb54e3fe7f62e1d9b047e6e529b4a4bb
Fulvio-P <mignano.1810986@studenti.uniroma1.it>
2020-05-22 14:26:55 +0200

    Rimossa variabile inutile

--- back-end/server/routes/opinion.js
+++ back-end/server/routes/opinion.js
@@ -3,11 +3,10 @@ const db = require("../db/index");
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    
-    let newRow;
+
     try {
         //prova a inserire il record nel db
-        newRow = await db.addOpinione(req.body.email, req.body.testo);    
+        await db.addOpinione(req.body.email, req.body.testo);    
     } catch (err) {
         //in caso di errore manda un messaggio adeguato
         console.error(`postgres error no. ${err.code}: ${err.message}`);
